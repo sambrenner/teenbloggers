a7d0b0b6f98523399f61423ce6b5c30a8bee7528
Sam Brenner <samjbrenner@gmail.com>
2014-03-13 17:59:54 -0400

    loading and parsing xml from lj server (testing)

--- routes/index.js
+++ routes/index.js
@@ -14,3 +14,21 @@ exports.pos = function(req, res){
   res.writeHead(200, {'Content-Type': 'application/json'});
   res.end(JSON.stringify(data));
 };
+
+exports.loadLJ = function(req, res) {
+  var request = require('request');
+  var xml2js = require('xml2js');
+  var parser = new xml2js.Parser();
+
+  request('http://' + req.params.username + '.livejournal.com/data/rss', function(e, r, b) {
+    if (!e && r.statusCode == 200) {
+      parser.parseString(b, function(e, r) {
+        var items = r.rss.channel[0].item;
+        for(var i=0; i<items.length; i++) {
+          var item = items[i];
+          console.log(item.description[0]);
+        }
+      });
+    }
+  });
+};
