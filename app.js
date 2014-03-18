09921eac3e644b2ebcccb8cc84799f4056a77159
Sam Brenner <samjbrenner@gmail.com>
2014-03-18 17:48:18 -0400

    extract questions

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ if ('development' == app.get('env')) {
 
 app.get('/pos/:text', routes.pos);
 app.get('/lj/:username', routes.loadLJ);
+app.get('/lj/:username/questions', routes.loadLJQuestions);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,8 @@
-var pos = require('pos');
-
-exports.pos = function(req, res){
-  var input = req.params.text;
-
-  var words = new pos.Lexer().lex(input);
-  var taggedWords = new pos.Tagger().tag(words);
-
-  var data = {
-    input: req.params.text,
-    tagged: taggedWords
-  };
-
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  res.end(JSON.stringify(data));
-};
-
-exports.loadLJ = function(req, res) {
+var getLJCorpus = function(username, callback) {
   var request = require('request');
   var stripper = require('htmlstrip-native');
   var xml2js = require('xml2js');
   var parser = new xml2js.Parser();
-  var username = req.params.username;
 
   request('http://' + username + '.livejournal.com/data/rss', function(e, r, b) {
     if (!e && r.statusCode == 200) {
@@ -45,9 +27,50 @@ exports.loadLJ = function(req, res) {
         ljInfo.username = username;
         ljInfo.corpus = corpus;
 
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify(ljInfo));
+        callback(ljInfo);
       });
     }
+  });
+};
+
+var extractQuestions = function(corpus) {
+  var questions = corpus.match(/[^.!?"]+\?/g);
+
+  for (var i = 0; i < questions.length; i++) {
+    questions[i] = questions[i].trim();
+  };
+
+  return questions;
+};
+
+exports.pos = function(req, res){
+  var pos = require('pos');
+  var input = req.params.text;
+
+  var words = new pos.Lexer().lex(input);
+  var taggedWords = new pos.Tagger().tag(words);
+
+  var data = {
+    input: req.params.text,
+    tagged: taggedWords
+  };
+
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.end(JSON.stringify(data));
+};
+
+exports.loadLJ = function(req, res) {
+  getLJCorpus(req.params.username, function(data) {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(data));
+  });
+};
+
+exports.loadLJQuestions = function(req, res) {
+  getLJCorpus(req.params.username, function(data) {
+    data.questions = extractQuestions(data.corpus);
+
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(data));
   });
 };
