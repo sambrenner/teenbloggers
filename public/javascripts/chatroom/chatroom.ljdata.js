56882d9c737ab4a838f5209d98d5488cf0edc3d0
Sam Brenner <samjbrenner@gmail.com>
2014-03-24 20:03:29 -0400

    data model for lj user data

---
+++ public/javascripts/chatroom/chatroom.ljdata.js
@@ -0,0 +1,16 @@
+var chatroom = chatroom || {};
+
+chatroom.ljdata = (function (window, document) {
+  var _data; 
+
+  var self = {
+    init: function() {
+      
+    }, 
+    set data(data) {
+      _data = data;
+    }
+  }; 
+
+  return self;
+})(this, this.document);
